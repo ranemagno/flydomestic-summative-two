f6c5097813e188991ecbc04a2d1080185ea55fe4
Rane Magno <270030927@bun2-206-23.acg.ac.nz>
2021-06-08 11:40:00 +1200

    first commit

---
+++ js/data.js
@@ -0,0 +1,30 @@
+var data = {
+  hostel: {
+    groupSize: '1',
+    priceperNight: 30,
+    minNights: 1,
+    maxNights: 10,
+    mealsProvided: ['Breakfast'];
+  },
+  hotel: {
+    groupSize: '1-2',
+    pricePerNight: 157,
+    minNights: 1,
+    maxNights: 10,
+    mealsProvided: ['Breakfast', 'Lunch', 'Dinner'];
+  },
+  motel: {
+    groupSize: '2-4',
+    pricePerNight: 90,
+    minNights: 3,
+    maxNights: 10,
+    mealsProvided: ['Breakfast', 'Lunch'];
+  },
+  house: {
+    groupSize: '1-4',
+    pricePerNight: 240,
+    minNights: 2,
+    maxNights: 15,
+    mealsProvided: [];
+  }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# FLYDOMESTIC
+# Production Tools
+# Validation Statement
+# Javascript Style Guide
